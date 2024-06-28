efcf8455eb7f04a5a6d02c34c969ce0373a74247
Mag1sstr <danilkarachev234@mail.ru>
2024-06-29 03:18:24 +0500

    add input

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -16,6 +16,7 @@ const data = [
 
 export default function Home() {
   const [todos, setTodos] = useState(data);
+  let [inputValue, setInputValue] = useState("");
   function changeTodo(id) {
     const copy = [...todos];
     const current = copy.find((t) => t._id === id);
@@ -27,6 +28,27 @@ export default function Home() {
   return (
     <div className=" text-white w-4/5 mx-auto">
       <h1 className="text-2xl font-bold text-center mb-10">Todo app</h1>
+      <input
+        style={{ color: "white" }}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key == "Enter") {
+            let newData = [...todos];
+            let newObj = {
+              _id: new Date(),
+              title: inputValue,
+              isCompleted: false,
+            };
+            newData.unshift(newObj);
+            setTodos(newData);
+            setInputValue("");
+          }
+        }}
+        className=" caret-white mb-4 rounded-2xl bg-gray-800 p-5 w-full outline-none focus:pl-7 transition-all duration-150"
+        type="text"
+        placeholder="Введите название задачи"
+      />
       {todos.map((todo) => (
         <TodoItem
           key={todo._id}
