0bfc6c0f6117e541c6c43cb7456c0d678795d1ed
Mag1sstr <danilkarachev234@mail.ru>
2024-11-06 05:55:59 +0500

    add edit todo title

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -6,11 +6,13 @@ const data = [
     _id: 1,
     title: "Make a website",
     isCompleted: false,
+    click: false,
   },
   {
     _id: 2,
     title: "Drink coffee",
     isCompleted: false,
+    click: false,
   },
 ];
 
@@ -24,9 +26,10 @@ export default function Home() {
     setTodos(copy);
   }
   const removeTodo = (id) => setTodos([...todos].filter((t) => t._id !== id));
+  console.log(todos);
 
   return (
-    <div className=" text-white w-4/5 mx-auto">
+    <div className=" text-white  w-4/5 mx-auto">
       <h1 className="text-2xl font-bold text-center mb-10">Todo app</h1>
       <input
         style={{ color: "white" }}
@@ -36,9 +39,10 @@ export default function Home() {
           if (e.key == "Enter") {
             let newData = [...todos];
             let newObj = {
-              _id: new Date(),
+              _id: Math.random(),
               title: inputValue,
               isCompleted: false,
+              click: false,
             };
             newData.unshift(newObj);
             setTodos(newData);
@@ -49,10 +53,15 @@ export default function Home() {
         type="text"
         placeholder="Введите название задачи"
       />
-      {todos.map((todo) => (
+      {todos.map((todo, index) => (
         <TodoItem
           key={todo._id}
           todo={todo}
+          click={todo.click}
+          todos={todos}
+          setTodos={setTodos}
+          index={index}
+          title={todo.title}
           changeTodo={changeTodo}
           removeTodo={removeTodo}
         />
--- src/components/screens/item/TodoItem.jsx
+++ src/components/screens/item/TodoItem.jsx
@@ -1,23 +1,82 @@
+import { useState } from "react";
 import Check from "./Check";
 import { FaTrash } from "react-icons/fa";
+import { TiTickOutline } from "react-icons/ti";
+import { MdEdit } from "react-icons/md";
 
-export default function TodoItem({ todo, changeTodo, removeTodo }) {
+export default function TodoItem({
+  todo,
+  changeTodo,
+  removeTodo,
+  todos,
+  index,
+  setTodos,
+  click,
+  title,
+}) {
+  const [edit, setEdit] = useState(title);
   return (
     <div className="flex items-center justify-between gap-3 mb-4 rounded-2xl bg-gray-800 p-5 w-full">
-      <button
-        onClick={() => {
-          changeTodo(todo._id);
-        }}
-        className="flex items-center gap-3"
-      >
-        <Check isCompleted={todo.isCompleted} />
-        <span className={todo.isCompleted ? "line-through" : ""}>
-          {todo.title}
-        </span>
-      </button>
-      <button onClick={() => removeTodo(todo._id)}>
-        <FaTrash size={20} className="text-gray-500 hover:text-red-500" />
-      </button>
+      {click ? (
+        <div className="flex items-center gap-4">
+          <input
+            className="text-white bg-slate-600 py-2 px-4 rounded-lg outline-none"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            onKeyDown={(e) => {
+              if (e.key == "Enter") {
+                let copy = [...todos];
+                copy[index].title = edit;
+                copy[index].click = !copy[index].click;
+                setTodos(copy);
+              }
+            }}
+            value={edit}
+            onChange={(e) => setEdit(e.target.value)}
+            type="text"
+          />
+          <TiTickOutline
+            onClick={() => {
+              let copy = [...todos];
+              copy[index].title = edit;
+              copy[index].click = !copy[index].click;
+              setTodos(copy);
+            }}
+            className="bg-pink-400 rounded-lg cursor-pointer"
+            size={28}
+          />
+        </div>
+      ) : (
+        <button
+          onClick={() => {
+            changeTodo(todo._id);
+          }}
+          className="flex items-center gap-3"
+        >
+          <Check isCompleted={todo.isCompleted} />
+          <span className={todo.isCompleted ? "line-through text-red-400" : ""}>
+            {todo.title}
+          </span>
+        </button>
+      )}
+      <div className="flex items-center gap-4">
+        <MdEdit
+          size={20}
+          className="text-blue-500 cursor-pointer"
+          onClick={() => {
+            let copy = [...todos];
+            copy[index].click = !copy[index].click;
+            setTodos(copy);
+          }}
+        />
+        <div className="w-[1px] h-6 bg-white"></div>
+        <FaTrash
+          onClick={() => removeTodo(todo._id)}
+          size={20}
+          className="text-gray-500 hover:text-red-500"
+        />
+      </div>
     </div>
   );
 }
